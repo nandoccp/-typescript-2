b5b8a7a9a86928601887fd61d6d66f25701bc557
Fernando Silva <nandoccp@gmail.com>
2021-12-19 16:36:53 -0300

    commit inicial

---
+++ app/views/view.ts
@@ -0,0 +1,28 @@
+export abstract class View<T> {
+    protected elemento: HTMLElement;
+    private escapar: boolean = false;
+
+    constructor(selector: string, escapar?: boolean) {
+        const elemento = document.querySelector(selector); 
+        
+        if (elemento) {
+            this.elemento = elemento as HTMLElement;
+        }
+
+        if (escapar) {
+            this.escapar = escapar;
+        }
+    }   
+
+    public update(model: T): void {
+        let template = this.template(model);
+        if (this.escapar) {
+            template = template
+                .replace(/<script>[\s\S]*?<\/script>/, '');
+        }
+
+        this.elemento.innerHTML = template;
+    }
+
+    protected abstract template(model: T): string;
+}
---
+++ dist/js/views/view.js
@@ -0,0 +1,20 @@
+export class View {
+    constructor(selector, escapar) {
+        this.escapar = false;
+        const elemento = document.querySelector(selector);
+        if (elemento) {
+            this.elemento = elemento;
+        }
+        if (escapar) {
+            this.escapar = escapar;
+        }
+    }
+    update(model) {
+        let template = this.template(model);
+        if (this.escapar) {
+            template = template
+                .replace(/<script>[\s\S]*?<\/script>/, '');
+        }
+        this.elemento.innerHTML = template;
+    }
+}
